8d7159e9462b65273ddc877665cd548f9df5a451
ushumpei <shumpei.uzawa@gmail.com>
2018-02-07 11:47:42 +0800

    Test to make it sure that gotoThread doesn't work.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import Botkit from 'botkit'
 
-const controller = Botkit.consolebot({ debug: true })
+const controller = Botkit.consolebot({ debug: false })
 
 controller.on('message_received', (bot, message) => {
   bot.reply(message, 'Hello world!')
   bot.startConversation(message, (err, convo) => {
     if (err) throw err
     convo.say('Conversation start')
+    convo.addMessage('Thread 1', '1')
+    convo.gotoThread('1')
+    convo.addMessage('Thread 2', '2')
+    convo.gotoThread('2')
+    convo.addMessage('Thread 3', '3')
+    convo.gotoThread('3')
     convo.say('Conversation end')
   })
 })
