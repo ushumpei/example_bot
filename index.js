4b2297bfc0d5a52df732f590220e2839b3a862a6
ushumpei <shumpei.uzawa@gmail.com>
2018-02-07 11:37:27 +0800

    Create conversation to learn it.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const controller = Botkit.consolebot({ debug: true })
 
 controller.on('message_received', (bot, message) => {
   bot.reply(message, 'Hello world!')
+  bot.startConversation(message, (err, convo) => {
+    if (err) throw err
+    convo.say('Conversation start')
+    convo.say('Conversation end')
+  })
 })
 
 controller.spawn()
